042a9a787dba089763bc2580628883858204a6cb
Bri <brionnafranklin@yahoo.com>
2020-08-20 09:48:16 -0500

    finished timestep tutorial

--- PhysicsScene/PhysicsSceneApp.cpp
+++ PhysicsScene/PhysicsSceneApp.cpp
@@ -2,6 +2,8 @@
 #include "Texture.h"
 #include "Font.h"
 #include "Input.h"
+#include <Gizmos.h>
+#include <glm\ext.hpp>
 
 PhysicsSceneApp::PhysicsSceneApp() {
 
@@ -12,8 +14,14 @@ PhysicsSceneApp::~PhysicsSceneApp() {
 }
 
 bool PhysicsSceneApp::startup() {
+
+	// increase the 2d line count to maximize the number of objects we can draw
+	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
 	
 	m_2dRenderer = new aie::Renderer2D();
+
+	m_physicsScene = new PhysicsScene();
+	m_physicsScene->setTimeStep(0.01f);
 
 	// TODO: remember to change this when redistributing a build!
 	// the following path would be used instead: "./font/consolas.ttf"
@@ -33,6 +41,11 @@ void PhysicsSceneApp::update(float deltaTime) {
 	// input example
 	aie::Input* input = aie::Input::getInstance();
 
+	aie::Gizmos::clear();
+
+	m_physicsScene->update(deltaTime);
+	m_physicsScene->updateGizmos();
+
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
@@ -47,6 +60,9 @@ void PhysicsSceneApp::draw() {
 	m_2dRenderer->begin();
 
 	// draw your stuff here!
+	static float aspectRatio = 16 / 9.0f;
+	aie::Gizmos::draw2D(glm::ortho<float>(-100, 100,
+		-100 / aspectRatio, 100 / aspectRatio, -1.0f, 1.0f));
 	
 	// output some text, uses the last used colour
 	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
